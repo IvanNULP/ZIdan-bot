c54d8252fd45333bc4e31d981812c735d50af386
IvanNULP <86712043+IvanNULP@users.noreply.github.com>
2025-04-12 19:30:45 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 
-# Вставь токен от BotFather
-BOT_TOKEN = '8068457846:AAFn2T1xdzUswHhMWwwWVMBhA6tDd_U6qIs'
+# Отримуємо токен з середовища (а не прописуємо напряму!)
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-# Реальный Telegram user_id пользователя, которому разрешено писать
-AUTHORIZED_USER_ID = 384210176
+# Телеграм user_id дозволеного користувача
+AUTHORIZED_USER_ID = 384210176  # Заміни на реального користувача
 
-# Ссылка на видео
+# Посилання на відео, яке бот надсилає у відповідь
 VIDEO_LINK = 'https://t.me/c/1294934054/299430'
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.effective_user.id == AUTHORIZED_USER_ID:
         await update.message.reply_text(VIDEO_LINK)
 
-app = ApplicationBuilder().token(BOT_TOKEN).build()
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+if __name__ == '__main__':
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
-print("Бот запущен...")
-app.run_polling()
+    print("Бот запущено на Render...")
+    app.run_polling()
